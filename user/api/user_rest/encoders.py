b853477b1fdee17378231e40d466068a890967d5
Joshua <jpe7619@gmail.com>
2023-03-14 17:14:01 -0700

    Revert "reverted to ver with working migration"
    This reverts commit 0d56524210f21690e0352f921f8b2a1078738153.

--- user/api/user_rest/encoders.py
+++ user/api/user_rest/encoders.py
@@ -1,24 +1,6 @@
 from common.json import ModelEncoder
 from .models import User
 from common.json import ModelEncoder
-from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models.fields import DateField
-
-class CustomJSONEncoder_User(DjangoJSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, DateField):
-            return obj.strftime('%Y-%m-%d')
-        if isinstance(obj, User):
-            return {
-                'name': obj.name,
-                'birthday': obj.birthday,
-                'photo': obj.photo,
-                'email': obj.email,
-                'host_status': obj.host_status,
-                'join_date': obj.join_date
-            }
-        return super().default(obj)
-
 
 class UserEncoder(ModelEncoder):
     model = User
@@ -27,7 +9,6 @@ class UserEncoder(ModelEncoder):
         "name",
         "birthday",
         "photo",
-        "email",
         "host_status",
         "join_date",
     ]
--- user/api/user_rest/views.py
+++ user/api/user_rest/views.py
@@ -2,28 +2,23 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from common.json import ModelEncoder
 import json
-from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models.fields import DateField
 from django.views.decorators.http import require_http_methods
 from .models import User
-from .encoders import UserEncoder, CustomJSONEncoder_User
-
-
-
+from .encoders import UserEncoder
 
 @require_http_methods(["GET", "POST"])
 def api_user_list(request):
     if request.method == "GET":
         users = User.objects.all()
         return JsonResponse(
-            {"users": list(users)}, encoder=CustomJSONEncoder_User, safe=False
+            {"users": users}, encoder=UserEncoder, safe=False
         )
     else:
         try:
             content = json.loads(request.body)
             user = User.objects.create(**content)
             return JsonResponse(
-                {"users": user}, encoder=CustomJSONEncoder_User, safe=False
+                {"users": user}, encoder=UserEncoder, safe=False
             )
         except:
             response = JsonResponse({"message": "Invalid User Info"})
