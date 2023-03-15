ee94ca9125688046d0bc847e3db4d96552d2b859
Joshua <jpe7619@gmail.com>
2023-03-14 18:31:06 -0700

    updated encoder for relevant information

--- user/api/user_rest/encoders.py
+++ user/api/user_rest/encoders.py
@@ -10,12 +10,15 @@ class CustomJSONEncoder_User(DjangoJSONEncoder):
             return obj.strftime('%Y-%m-%d')
         if isinstance(obj, User):
             return {
-                'name': obj.name,
-                'birthday': obj.birthday,
                 'photo': obj.photo,
                 'email': obj.email,
                 'host_status': obj.host_status,
-                'join_date': obj.join_date
+                'join_date': obj.join_date,
+                'username': obj.username,
+                'first_name': obj.first_name,
+                'last_name': obj.last_name,
+                'birthday': obj.birthday,
+                'password': obj.password
             }
         return super().default(obj)
 
